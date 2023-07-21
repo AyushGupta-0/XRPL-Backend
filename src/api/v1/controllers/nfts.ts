e10570f9b1f91fe03765a4e11f6815fc815bdf91
Karan Agrawal <karan.agr9034@gmail.com>
2023-07-21 10:53:26 +0530

    NFT Fetch/Mint

--- src/api/v1/controllers/nfts.ts
+++ src/api/v1/controllers/nfts.ts
@@ -1,12 +1,49 @@
-import { Request, Response } from 'express'
+import { Response } from 'express'
 import db from '../helpers/firebase'
+import xumm from "../helpers/xumm"
+import { fetchNFTs } from '../helpers/xrpl'
 import uploadToIPFS from '../helpers/ipfs'
+import ApiRequest from '../interfaces/ApiRequest'
+import { PayloadAndSubscription, XummPostPayloadBodyJson } from 'xumm-sdk/dist/src/types'
+import {convertStringToHex} from 'xrpl'
 
-export const getAllNFTs = (req: Request, res: Response) => {
+export const getAllNFTs = (req: ApiRequest, res: Response) => {
     db.collection('nfts').onSnapshot(snapshot => {
         let resData = snapshot.docs.map(doc => {
             return {id:doc.id, ...doc.data()}
         })
         res.json(resData)
     })
+}
+
+export const getAccountNFTs = async (req: ApiRequest, res: Response) => {
+    if((await db.collection('users').doc(req.params.account).get()).exists){
+        const nfts = (await fetchNFTs(req.params.account))
+        res.status(200).json({status: 'success', nfts})
+    }else{
+        res.status(404).json({status: 'failed', message: 'Account not found'})
+    }
+}
+
+export const getNFTDetails = (req: ApiRequest, res: Response) => {
+
+}
+
+export const createNFT = async (req: ApiRequest, res: Response) => {
+    const uri = await uploadToIPFS(req.file?.buffer)
+    const transaction: XummPostPayloadBodyJson = {
+        txjson: {
+            TransactionType: "NFTokenMint",
+            URI: convertStringToHex(`ipfs://${uri}`),
+            Flags: 8,
+            NFTokenTaxon: 0,
+        },
+        user_token: req.user.xummToken
+    }
+    const subscription: PayloadAndSubscription | undefined = await xumm.payload?.createAndSubscribe(transaction, async (event) => {
+		if(event.data.signed){
+            res.json({status: 'success', message: 'NFT created successfully'})
+        }
+    })
+	res.json({uuid: subscription?.created.uuid, url: `https://xumm.app/sign/${subscription?.created.uuid}`, wss: `wss://xumm.app/sign/${subscription?.created.uuid}`})
 }
--- src/api/v1/routes/nfts.ts
+++ src/api/v1/routes/nfts.ts
@@ -1,8 +1,15 @@
-import {Router} from 'express'
-import {getAllNFTs} from '../controllers/nfts'
+import { Router } from 'express'
+import { getAllNFTs, getAccountNFTs, getNFTDetails, createNFT } from '../controllers/nfts'
+import verifyToken from '../middlewares/verifyJWT'
+import multer, {Multer} from 'multer'
 const router: Router = Router()
+const upload: Multer = multer()
 
-router.get('/profile', getAllNFTs)
+router.get('/', getAllNFTs)
+router.post('/mint', upload.single('nftFile'), verifyToken, createNFT)
+router.get('/:account', getAccountNFTs)
+
+router.get('/:nftid', getNFTDetails)
 
 
 export default router
